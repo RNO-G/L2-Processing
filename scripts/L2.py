333bb9e34ddcb84b167875e0edba1be5f1824ce1
Aishwarya Vijai - UMD Grad Student <avijai@condor.private.pa.umd.edu>
2025-02-03 13:46:37 -0500

    L2 script modifications

--- scripts/L2.py
+++ scripts/L2.py
@@ -1,6 +1,7 @@
 import argparse
 import preprocessor 
 import NuRadioReco.modules.channelSignalReconstructor
+import NuRadioReco.modules.RNO_G.dataProviderRNOG
 import snr
 import rpr
 import reco
@@ -9,8 +10,10 @@ import hilbert
 import impulsivity
 import utils
 import NuRadioReco.detector.RNO_G.rnog_detector
-from NuRadioReco.framework.parameters import eventParameters as ep
+from NuRadioReco.framework.parameters import eventParametersRNOG as ep
 import logging
+import NuRadioReco.modules.RNO_G.channelGlitchDetector
+from NuRadioReco.examples.RNOG.processing import process_event
 
 detectorpath = "/data/i3store/users/avijai/RNO_season_2023.json"
 csw_info = {
@@ -43,26 +46,20 @@ ttcs = utils.load_ttcs(mappath, csw_info["ALL"])
 
 det = NuRadioReco.detector.RNO_G.rnog_detector.Detector()
 
-channelSignalReconstructor = NuRadioReco.modules.channelSignalReconstructor.channelSignalReconstructor()
-channelSignalReconstructor.begin()
 
-preprocessor = preprocessor.Preprocessor()
-preprocessor.begin()
+dataProviderRNOG = NuRadioReco.modules.RNO_G.dataProviderRNOG.dataProvideRNOG()
+dataProviderRNOG.begin(files = filename, det = det)
+for idx, event in enumerate(dataProviderRNOG.run()):
+    process_event(event, det)
 
-
-readRNOGDataMattak = NuRadioReco.modules.io.RNO_G.readRNOGDataMattak.readRNOGData(log_level=logging.INFO)
-readRNOGDataMattak.begin(filename, read_calibrated_data = False, convert_to_voltage = True, overwrite_sampling_rate = 3.2)
-
-for idx, event in enumerate(readRNOGDataMattak.run()):
     station = event.get_station(station_id)
-    preprocessor.run(event, station, det)
 
     det.update(station.get_station_time())
 
     maxcorr_point, maxcorr, score, t_ab, surf_corr_ratio, max_surf_corr = reco.run(event, station, detectorpath, station_id, csw_info["ALL"], do_envelope, res, mappath, ttcs) 
-    channelSignalReconstructor.run(event, station, det)
-    avg_snr = event.avg_SNR()
-    avg_rpr = event.avg_RPR()
+    avg_snr = event.avg_SNR_all_stations()
+    avg_rpr = event.avg_RPR_all_stations()
+    
     
     csw_rpr = {}
     csw_snr = {}
@@ -78,6 +75,9 @@ for idx, event in enumerate(readRNOGDataMattak.run()):
         csw_impulsivity[chan_combo] = impulsivity.calculate_impulsivity_measures(csw_values)
 
     
+    event.add_parameter_type(ep)
+    event[ep.avg_snr] = avg_snr
+    event[ep.avg_rpr] = avg_rpr
     event[ep.max_corr_coords] = maxcorr_point 
     event[ep.max_corr] = maxcorr
     event[ep.csw_snr] = csw_snr
@@ -86,4 +86,5 @@ for idx, event in enumerate(readRNOGDataMattak.run()):
     event[ep.csw_impulsivity] = csw_impulsivity 
     event[ep.surf_corr_ratio] = surf_corr_ratio
     event[ep.max_surf_corr] = max_surf_corr
-
+    
+    print(event.get_id())
